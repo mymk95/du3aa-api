216efafbc70c4cf5fb9303236b95d6aa21940ee7
mfilkawi <m.filkawi@gmail.com>
2020-10-17 13:50:16 +0300

    twitter API route with auth added

--- routes/twitterRoutes.js
+++ routes/twitterRoutes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const twitterController = require('../controllers/twitterController');
+const authenticateToken = require('../middleware/jwt');
 
 router.post('/oauth', twitterController.oauth);
 router.get('/callback', twitterController.callback);
 router.get('/count', twitterController.count);
+
+router.get('/subs', authenticateToken,twitterController.index);
 
 module.exports = router;

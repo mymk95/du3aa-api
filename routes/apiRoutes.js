b96e3b8bbb56d7fe561e69575c5b92d2d80117f1
mymk95 <m.filkawi@gmail.com>
2020-11-10 18:37:22 +0300

    url params renamed to prayer_id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,10 +16,10 @@ router.get('/random', prayerController.random)
 router.get('/count', prayerController.count)
 
 router.get('/prayer', prayerController.index)
-router.get('/prayer/:du3aa_id', prayerController.view)
+router.get('/prayer/:prayer_id', prayerController.view)
 
 router.post('/prayer', authenticateToken, prayerController.new)
-router.put('/prayer/:du3aa_id', authenticateToken, prayerController.update)
-router.delete('/prayer/:du3aa_id', authenticateToken, prayerController.delete)
+router.put('/prayer/:prayer_id', authenticateToken, prayerController.update)
+router.delete('/prayer/:prayer_id', authenticateToken, prayerController.delete)
 
 module.exports = router

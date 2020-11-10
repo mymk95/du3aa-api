7510a0ddb1a4fa100dce25aa89a1e40cd3c91955
mymk95 <m.filkawi@gmail.com>
2020-11-10 18:25:54 +0300

    schema renamed to prayerSchema

--- models/du3aaModel.js
+++ models/du3aaModel.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-const du3aaSchema = new mongoose.Schema({
+const prayerSchema = new mongoose.Schema({
   du3aa: {
     type: String,
     required: true
   }
 })
 
-const Du3aaModel = module.exports = mongoose.model('du3aa', du3aaSchema)
-module.exports = mongoose.model('du3aa', du3aaSchema)
+const Du3aaModel = module.exports = mongoose.model('du3aa', prayerSchema)
+module.exports = mongoose.model('du3aa', prayerSchema)
 
 module.exports.get = function (callback, limit) {
   Du3aaModel.find(callback).limit(limit)

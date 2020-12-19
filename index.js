ed35370c37a344fd3f76c7bd73e0a7b9b0adb5b5
mymk95 <m.filkawi@gmail.com>
2020-12-19 17:28:26 +0300

    main worker project

---
+++ index.js
@@ -0,0 +1,38 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond with a random prayer
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+
+  try {
+    const prayer = JSON.parse(
+      await (await fetch('https://raw.githubusercontent.com/mymk95/sabah/main/quotes.json')).text()
+    )
+
+    const randomPrayer = prayer[Math.floor(Math.random() * prayer.length)];
+
+    return new Response(JSON.stringify({ prayer: randomPrayer }), {
+      status: 200,
+      headers: { 
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Content-Type': 'application/json' 
+      },
+    })
+
+  } catch (error) {
+    return new Response(JSON.stringify({ 'status': '500', 'error': 'Internal Server Error' }), {
+      status: 500,
+      headers: { 
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Content-Type': 'application/json' 
+      },
+    })
+  }
+
+}

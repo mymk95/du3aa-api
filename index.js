6cf772db6a7725f0d812e5a7ef5a96f53dc60388
mymk95 <m.filkawi@gmail.com>
2020-12-19 19:29:10 +0300

    accepts text content type

--- index.js
+++ index.js
@@ -9,28 +9,56 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
 
   try {
+    const url = new URL(request.url)
+    var formatParameter = null
+    
+    if (url.searchParams.get('format')) {
+      formatParameter = url.searchParams.get('format').toLocaleLowerCase().split('/')[0]
+      
+			if (formatParameter !== 'json' && formatParameter !== 'text') {
+				return new Response(JSON.stringify({ status: 400, error: 'Invalid format parameter' }), {
+					status: 400,
+					headers: {
+						'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Content-Type': 'application/json'
+					}
+				})
+      }
+		}
+
     const prayer = JSON.parse(
       await (await fetch('https://raw.githubusercontent.com/mymk95/du3aa-api/workers/prayers.json')).text()
     )
+    const randomPrayer = prayer[Math.floor(Math.random() * prayer.length)]
 
-    const randomPrayer = prayer[Math.floor(Math.random() * prayer.length)];
-
-    return new Response(JSON.stringify({ prayer: randomPrayer }), {
-      status: 200,
-      headers: { 
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Content-Type': 'application/json' 
-      },
-    })
+    if (formatParameter === 'text') {
+      return new Response(randomPrayer, {
+        status: 200,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET',
+					'Content-Type': 'text/plain;charset=utf-8'
+				}
+      })
+		} else {
+			return new Response(JSON.stringify({ prayer: randomPrayer }), {
+        status: 200,
+        headers: { 
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET',
+          'Content-Type': 'application/json'
+        },
+      })
+		}
 
   } catch (error) {
-    return new Response(JSON.stringify({ 'status': '500', 'error': 'Internal Server Error' }), {
+    return new Response(JSON.stringify({ status: '500', error: 'Internal Server Error' }), {
       status: 500,
       headers: { 
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET',
-        'Content-Type': 'application/json' 
+        'Content-Type': 'application/json'
       },
     })
   }

d644ac48b3916285c01c4ced8c84203144c13b71
mymk95 <m.filkawi@gmail.com>
2020-12-22 16:42:38 +0300

    whitespaces remove script added

--- filter.py
+++ filter.py
@@ -1,9 +1,19 @@
+'''
+fliter.py
+Removes duplicated prayers and whitespaces in prayers.json
+'''
+
 import json
 
 with open('prayers.json') as f:
   data = json.load(f)
 
 filtered = list(dict.fromkeys(data))
+filtered_ws = list()
+
+for f in filtered:
+  f = " ".join(f.split())
+  filtered_ws.append(f)
 
 with open('prayers.json', 'w', encoding='utf-8') as json_file:
-  json.dump(filtered, json_file, ensure_ascii = False, indent = 2)
+  json.dump(filtered_ws, json_file, ensure_ascii = False, indent = 2)

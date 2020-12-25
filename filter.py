0bf7c0ce4fa6c8419d931c3f192417d97d9abd4b
mymk95 <m.filkawi@gmail.com>
2020-12-25 14:27:57 +0300

    add a prayer function created

--- filter.py
+++ filter.py
@@ -19,6 +19,15 @@ def filter():
   with open('prayers.json', 'w', encoding='utf-8') as json_file:
     json.dump(filtered_ws, json_file, ensure_ascii = False, indent = 2)
 
+def add(prayer):
+  with open('prayers.json') as f:
+    data = json.load(f)
+  
+  data.append(prayer)
+
+  with open('prayers.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, ensure_ascii = False, indent = 2)
+
 if __name__ == "__main__":
   try:
     choice = input('please choose an option:\n1- Filter prayers.json\n2- Add a prayer\nUser choice: ')
@@ -28,7 +37,10 @@ if __name__ == "__main__":
       print('Done')
 
     elif choice == '2':
-      print('Add')
+      print('Adding...')
+      prayer = input('Prayer: ')
+      add(prayer)
+      print('done')
 
     else:
       print('Choose wisely :)')

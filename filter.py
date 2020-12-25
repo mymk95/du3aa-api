899aaa4a6e763d83d2e4bf2441c118a6955badf8
mymk95 <m.filkawi@gmail.com>
2020-12-25 14:14:01 +0300

    filter proccess wrapped into a function

--- filter.py
+++ filter.py
@@ -5,15 +5,16 @@ Removes duplicated prayers and whitespaces in prayers.json
 
 import json
 
-with open('prayers.json') as f:
-  data = json.load(f)
+def filter():
+  with open('prayers.json') as f:
+    data = json.load(f)
 
-filtered = list(dict.fromkeys(data))
-filtered_ws = list()
+  filtered = list(dict.fromkeys(data))
+  filtered_ws = list()
 
-for f in filtered:
-  f = " ".join(f.split())
-  filtered_ws.append(f)
+  for f in filtered:
+    f = " ".join(f.split())
+    filtered_ws.append(f)
 
-with open('prayers.json', 'w', encoding='utf-8') as json_file:
-  json.dump(filtered_ws, json_file, ensure_ascii = False, indent = 2)
+  with open('prayers.json', 'w', encoding='utf-8') as json_file:
+    json.dump(filtered_ws, json_file, ensure_ascii = False, indent = 2)

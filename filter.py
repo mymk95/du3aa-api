c0d24b0c5824d411136c28042a98ddcccb6b858d
mymk95 <m.filkawi@gmail.com>
2020-12-25 14:20:42 +0300

    menu added

--- filter.py
+++ filter.py
@@ -18,3 +18,20 @@ def filter():
 
   with open('prayers.json', 'w', encoding='utf-8') as json_file:
     json.dump(filtered_ws, json_file, ensure_ascii = False, indent = 2)
+
+if __name__ == "__main__":
+  try:
+    choice = input('please choose an option:\n1- Filter prayers.json\n2- Add a prayer\nUser choice: ')
+    if choice == '1':
+      print('Filtering ...')
+      filter()
+      print('Done')
+
+    elif choice == '2':
+      print('Add')
+
+    else:
+      print('Choose wisely :)')
+
+  except Exception as e:
+    print(e)
